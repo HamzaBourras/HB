f315ea92349a726db10c3e4ee3e5c58a8cce444a
HamzaBourras <124906874+HamzaBourras@users.noreply.github.com>
2023-10-24 23:13:26 +0100

    Delete Projet_Stage directory

--- Projet_Stage/Voiture/cartemaps.js
+++
@@ -1,9 +0,0 @@
- function afficherposition(){
-    var position=document.getElementById('pos').value;
-    var iframe=document.getElementById('iframe');
-
-        //construire l'url
-    var mapUrl = "https://www.google.com/maps?q=" + encodeURIComponent(position) + "&output=embed";
-
-    iframe.src=mapUrl;
- }
